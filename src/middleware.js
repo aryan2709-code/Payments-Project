efb29aed56466316061b535b4f86d61f9dd5195f
Aryan <bhardwaj7a@gmail.com>
2025-03-31 15:17:53 +0530

    Wrapped the middleware logic inside a try catch block

--- src/middleware.js
+++ src/middleware.js
@@ -4,16 +4,20 @@ import jwt from "jsonwebtoken";
 
 export const authMiddleware = async(req,res,next) => {
          const header = req.headers.authorization;
-         const decoded = jwt.verify(header , JWT_SECRET);
-         if(decoded)
-         {
-            req.userId = decoded.userId;
-            next();
-         }
-         else
-         {
-            res.status(403).json({
-                message : "You are not logged in"
-            })
-         }
+         try{
+            const decoded = jwt.verify(header , JWT_SECRET);
+            if(decoded)
+            {
+               req.userId = decoded.userId;
+               next();
+            }
+            else
+            {
+               res.status(403).json({
+                   message : "You are not logged in"
+               })
+            }
+         }catch(error){
+            console.error("There was some error: " , error)
+         }       
 }
